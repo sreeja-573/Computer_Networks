19410ffaf05c6f03032bd0d37134bc094b6ddd10
sreeja-573 <51990185+sreeja-573@users.noreply.github.com>
2021-01-31 11:20:51 +0530

    Add files via upload

---
+++ EncryptDecrypt.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, x;
+   char str[100];
+
+   printf("\nPlease enter a string:\t");
+   gets(str);
+
+   printf("\nMenu:\n");
+   printf("1.Encrypt the string.\n");
+   printf("2.Decrypt the string.\n");
+   printf("\nPlease choose following options:");
+   scanf("%d", &x);
+
+   
+   switch(x)
+   {
+   case 1:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] + 3;
+
+      printf("\nEncrypted string: %s\n", str);
+      break;
+
+   case 2:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] - 3; 
+
+      printf("\nDecrypted string: %s\n", str);
+      break;
+
+   default:
+      printf("\nError\n");
+   }
+   return 0;
+}
+
+/*
+Output:
+Please enter a string:  hello
+
+Menu:
+1.Encrypt the string.
+2.Decrypt the string.
+Please choose following options:1
+
+Encrypted string: khoor
+
+
+
+Please enter a string:  khoor
+
+Menu:
+1.Encrypt the string.
+2.Decrypt the string.
+
+Please choose following options:2
+
+Decrypted string: hello
+
+*/
